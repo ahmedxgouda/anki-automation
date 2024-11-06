910a3dfc541545cf0aae74bc530ec0699530ac0d
Ahmed Gouda <ahmedmosad405@gmail.com>
2024-11-06 19:55:23 +0200

    remove PDF conversion option from deck creation process

--- anki-automation.py
+++ anki-automation.py
@@ -1,17 +1,12 @@
 import base64
 import os
 import requests
-from pdf_to_images import pdf_to_images
 import re
 
 def main():
     # receive the deck folder path
     deckFolder = input('Enter the deck folder path: ')
-    convertAndAddPdf = input('Do you want to convert PDFs to images and add them to Anki? (y/n): ')
-    while convertAndAddPdf.lower() not in ['y', 'n']:
-        convertAndAddPdf = input('Please enter y or n: ')
-    toConvert = convertAndAddPdf.lower() == 'y'
-    createDeckFromFolder(deckFolder, toConvert)
+    createDeckFromFolder(deckFolder)
     
     
 def addNoteWithImage(deckName, imagePath):
@@ -69,7 +64,7 @@ def createDeck(deckName):
     else:
         print(f"Deck '{deckName}' already exists.")
         
-def createDeckFromFolder(deckFolder: str, convertPdf: bool):
+def createDeckFromFolder(deckFolder: str):
     folders = deckFolder.split(os.sep)
     folderBeforeDeck = os.sep.join(folders[0:len(folders) - 1])
     for root, dirs, files in os.walk(deckFolder):
@@ -82,21 +77,7 @@ def createDeckFromFolder(deckFolder: str, convertPdf: bool):
             if file.endswith((".png", ".jpg", ".jpeg", ".gif")):
                 image_path = os.path.join(root, file)
                 addNoteWithImage(deckPath, image_path)
-            elif convertPdf and file.endswith(".pdf"):
-                print(f"Adding images from PDF: {file}")
-                pdf_path = os.path.join(root, file)
-                output_folder = pdf_path.replace('.pdf', '')
-                # Create the deck path for the PDF
-                pdfDeckPath = deckPath + "::" + file.replace(".pdf", "")
-                createDeck(pdfDeckPath)
-                pdf_to_images(pdf_path, output_folder)
-                images = os.listdir(output_folder)
-                # sort by numbers from 1, 2, 3 and so on
-                images = sorted(images, key=lambda x: int(re.search(r'\d+', x).group()))
-                for image in images:
-                    image_path = os.path.join(output_folder, image)
-                    addNoteWithImage(pdfDeckPath, image_path)
     print("Done adding images.")
-    
+
 if __name__ == "__main__":
     main()
